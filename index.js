9621c1a03a109888f2b3a12c3ea8683f06acfede
Mahendra Rao <nkneelkumar@gmail.com>
2025-03-01 02:21:03 +0530

    changed here

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello, Mahendra the CI/CD KING!\n');
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
